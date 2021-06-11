378fec9d7d9474fd38be4783cb12bf6fbe114ba7
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-06-11 11:58:42 -0500

    Cargue inicial

---
+++ src/views/Inicio.vue
@@ -0,0 +1,240 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p Bienvenido al programa Tecnología en coordinación de procesos logísticos que permite la obtención del título Tecnólogo en coordinación de procesos logísticos, de acuerdo con el modelo de formación por proyectos del Servicio Nacional de Aprendizaje SENA, siendo parte de la red de logística y transporte. La duración del período total de formación del programa es de nueve (9) trimestres, equivalente a 3.984 horas de formación incluyendo etapa lectiva y productiva, diseñado para desarrollar competencias y brindar herramientas especializadas complementarias a aquellas características del nivel tecnológico.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+
+    p.mb-0 Las empresas interpretan la globalización de la economía, los mercados sin fronteras y la apertura económica como fenómenos económicos a escala mundial que ofrecen una oportunidad para dinamizar los mercados entre países y su éxito se relaciona con el alto desempeño logístico, que busca satisfacer las necesidades de las partes interesadas, en el momento oportuno, con la calidad esperada y costos óptimos. Colombia no se queda atrás en esta realidad y se hace evidente la necesidad de enfrentar esos desafíos, que, ante las exigencias comerciales de un mercado globalizado, hace necesario cualificar capital humano en logística que apropie modelos dinámicos e integradores, con el fin de optimizar los procesos logísticos.
+      br
+      br
+      | Con esa formación se espera apoyar ese crecimiento y la dinámica del mercado, formando tecnólogos que asuman esos retos expuestos y otros de los retos logísticos que encara el país como la ampliación de infraestructura y la eficiencia logística. Frente a ese desafío el Servicio Nacional de Aprendizaje dispone el programa de formación; Tecnología en coordinación de procesos logísticos para dar respuesta a las expectativas del sector educativo y productivo del país, adaptando las tendencias logísticas y tecnológicas actuales al proceso de formación, resaltando el enfoque de cadena de suministros, logística 4.0 e innovación en los procesos logísticos, que permitan la inmersión de los aprendices al contexto laboral y productivo del país con el propósito de impactar la competitividad de las empresas del sector de la logística en Colombia.    
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Competencias a<br>desarrollar
+
+    p.mb-4 Cada una de las competencias relacionadas, contribuye a la formación del Tecnólogo en coordinación de procesos logísticos, aportando las herramientas técnicas y humanas necesarias para un óptimo desempeño laboral. De esta manera, el Tecnólogo adquiere conocimientos, destrezas y habilidades que le permitirán ejecutar las funciones propias de la operación logística. Como se evidencia en los conocimientos de conceptos, principios, procesos y criterios de evaluación de cada una de las competencias consideradas en el programa de formación de la Tecnología en coordinación de procesos logísticos.
+
+    table
+      thead
+        tr
+          th(style="width:80%") Nombre de la norma de competencia laboral / Unidad de competencia
+          th.text-center(style="width:20%") C&oacute;digo NCL / UC
+      tbody
+        tr
+          td Atender clientes de acuerdo con el procedimiento de servicio y la normativa.
+          td.text-center 210601020
+        tr
+          td Calcular costos de operaci&oacute;n de acuerdo con los m&eacute;todos.
+          td.text-center 210303025
+        tr
+          td Controlar los inventarios seg&uacute;n los indicadores y los m&eacute;todos.
+          td.text-center 210101062
+        tr
+          td
+            | Controlar los procesos de importaci&oacute;n y exportaci&oacute;n seg&uacute;n la normativa y el acuerdo entre las partes.
+          td.text-center 210101029
+        tr
+          td Coordinar el flujo de mercanc&iacute;a seg&uacute;n el plan de operaci&oacute;n y los procedimientos.
+          td.text-center 210101075
+        tr
+          td Dirigir el talento humano de acuerdo con la normativa.
+          td.text-center 210201052
+        tr
+          td
+            | Dise&ntilde;ar &aacute;reas de almacenamiento seg&uacute;n estrategias de log&iacute;stica y sistemas de almacenaje.
+          td.text-center 210101069
+        tr
+          td Elaborar el plan de compras seg&uacute;n los m&eacute;todos de programaci&oacute;n y presupuesto.
+          td.text-center 210101071
+        tr
+          td
+            | Planear la integraci&oacute;n de cadenas de suministro seg&uacute;n la normativa y modelos de gesti&oacute;n.
+          td.text-center 210101067
+        tr
+          td
+            | Proyectar los requerimientos de los clientes seg&uacute;n los m&eacute;todos de compras y el pron&oacute;stico.
+          td.text-center 210101051
+        tr
+          td Supervisar la prestaci&oacute;n de los procesos de negocio seg&uacute;n el modelo de gesti&oacute;n.
+          td.text-center 260102013
+        tr
+          td
+            | Aplicaci&oacute;n de conocimientos de las ciencias naturales de acuerdo con las situaciones del contexto productivo y social.
+          td.text-center 220201501
+        tr
+          td
+            | Aplicar pr&aacute;cticas de protecci&oacute;n ambiental, seguridad y salud en el trabajo de acuerdo con las pol&iacute;ticas organizacionales y la normatividad vigente.
+          td.text-center 220601501
+        tr
+          td
+            | Desarrollar procesos de comunicaci&oacute;n eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal.
+          td.text-center 240201524
+        tr
+          td
+            | Ejercer derechos fundamentales del trabajo en el marco de la constituci&oacute;n pol&iacute;tica y los convenios internacionales.
+          td.text-center 210201501
+        tr
+          td
+            | Enrique Low Murtra-Interactuar en el contexto productivo y social de acuerdo con principios &eacute;ticos para la construcci&oacute;n de una cultura de paz.
+          td.text-center 240201526
+        tr
+          td
+            | Generar h&aacute;bitos saludables de vida mediante la aplicaci&oacute;n de programas de actividad f&iacute;sica en los contextos productivos y sociales.
+          td.text-center 230101507
+        tr
+          td
+            | Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos de los contextos productivo y social.
+          td.text-center 240201529
+        tr
+          td
+            | Interactuar en lengua inglesa de forma oral y escrita dentro de contextos sociales y laborales seg&uacute;n los criterios establecidos por el Marco Com&uacute;n Europeo de referencia para las lenguas.
+          td.text-center 240202501
+        tr
+          td Orientar la investigaci&oacute;n formativa seg&uacute;n los referentes t&eacute;cnicos.
+          td.text-center 240201064
+        tr
+          td
+            | Razonar cuantitativamente frente a situaciones susceptibles de ser abordadas de manera matem&aacute;tica en contextos laborales, sociales y personales.
+          td.text-center 240201528
+        tr
+          td
+            | Utilizar herramientas inform&aacute;ticas de acuerdo con las necesidades de manejo de informaci&oacute;n.
+          td.text-center 220501046
+        tr
+          td Resultado de aprendizaje de la inducci&oacute;n.
+          td.text-center 240201530
+        tr
+          td Resultados de aprendizaje de la etapa pr&aacute;ctica.
+          td.text-center 999999999
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Pefil de <br>ingreso
+
+    p.mb-4 Nivel académico adecuado para caracterizar al aspirante de acuerdo con el perfil de egreso: 
+    ul.fa-ul
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Secundaria, grado 11. Requiere certificación académica.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] No requiere formación para el trabajo y desarrollo humano.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Edad mínima definida por ley: 16 años.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Superar la prueba de aptitud y conocimiento.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Requisito adicional: Presentar resultado de la prueba o examen de estado de la educación media.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] No existe legislación que establezca restricciones de ingreso, a nivel de discapacidad física y cognitiva.
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Aspectos actitudinales, motivacionales o de interés: responsabilidad, compromiso, honestidad, lealtad, iniciativa, cumplimiento, colaboración, comunicación asertiva, buenas relaciones interpersonales, capacidad de trabajo en equipo, capacidad para resolución de conflictos, alta capacidad de adaptabilidad, capacidad de trabajo bajo presión, presentación personal acorde con sus funciones, capacidad de redacción de informes técnicos, organización para el manejo de información, seguimiento e interpretación de instrucciones, liderazgo de equipos de trabajo.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Pefil de <br>egreso
+
+    p Los egresados del programa contarán con un amplio dominio de saberes relacionados con el comportamiento de los mercados y serán solucionadores creativos de problemas que enfrentan las empresas en el desarrollo de las operaciones logísticas a nivel nacional e internacional, capaces de gestionar procesos y estrategias logísticas que permitan la sincronización de la cadena de abastecimiento, con habilidades en el uso de tecnologías de la información y comunicación como variable estratégica para la toma de decisiones, con conciencia ambiental, responsabilidad ética y social, garantizando una gestión dentro de la empresa.
+      br
+      br
+      | El perfil general del egresado tendrá las siguientes habilidades: coordinan, asignan y supervisan las actividades de empleados que realizan funciones de despacho, recepción, almacenamiento, distribución y mantenimiento de inventarios, materiales, repuestos y/o productos, tramite de compras y programación de itinerarios y personal; u operación de rampas del aeropuerto para el servicio vehicular y otras actividades. Están empleados por empresas del sector público o privado.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 6. Estrategia<br>metodológica
+
+    p.mb-4 Centrada en la construcción de la autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportada en la utilización de las Tecnologías de la Información y la Comunicación (TIC), integradas en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo, vinculan al aprendiz con la realidad cotidiana y con el desarrollo de las competencias. 
+      br
+      br
+      | Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:      
+
+    ul.fa-ul
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El instructor - tutor
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El entorno
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Las TIC
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El trabajo colaborativo
+
+    
+    
+
+
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.resultados-aprendizaje
+  &__item
+    margin-bottom: 20px
+    &:last-child
+      margin-bottom: 0
+
+.desarrollo-contenidos
+  &__item
+    display: flex
+    align-items: center
+    justify-content: space-between
+    margin-bottom: 25px
+    padding: 5px
+    border-radius: $base-border-radius
+
+    &__texto
+      display: flex
+      align-items: center
+
+    &__icono
+      display: block
+      background-color: $color-sistema-g
+      font-size: 20px
+      font-weight: $base-black-font-weight
+      text-align: center
+      padding: 10px
+      line-height: 1em
+      width: 40px
+      margin-right: 15px
+
+    &:last-child
+      margin-bottom: 0
+
+    &:hover
+      background-color: $color-sistema-g
+      font-weight: $base-black-font-weight
+      color: $color-sistema-a
+</style>
